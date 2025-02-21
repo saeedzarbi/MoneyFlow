550e976564677b181d5831547024f52b794ba79b
saeedzarbi <saeedzarbi95@gmail.com>
2025-02-22 01:18:12 +0330

    app settings

--- Mate/app.py
+++ Mate/app.py
@@ -32,7 +32,10 @@ app.register_blueprint(auth_bp, url_prefix='/')
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=5002, debug=False)
+
